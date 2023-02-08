024c70a491c17078351faed81e6ae525080c07f8
Michaela Ptáčková <miskha.cz@gmail.com>
2023-02-08 14:20:42 +0100

    úkol ze čtvrté lekce (2.2.2023)

---
+++ ctvrtyUkol.py
@@ -0,0 +1,21 @@
+PHONE_NUMBER = input("Zadejte telefonní číslo, kam chceš zprávu poslat: ")
+
+def number_validate(YOUR_NUMBER):
+    if len(PHONE_NUMBER) == 9:
+        return True
+    elif len(PHONE_NUMBER) == 13 and PHONE_NUMBER[0:4] == "+420":
+        return True
+    else:
+        return False
+print(number_validate(PHONE_NUMBER))
+
+def price_message(message):
+    cost = 3 * (len(message) // 180 + 1)
+    return cost
+
+if number_validate(PHONE_NUMBER):
+  message = input("Zadejte zprávu: ")
+  cost = price_message(message)
+  print(f"Cena za SMS je {cost} Kč.")
+else:
+  print("Chybné telefonní číslo!")
